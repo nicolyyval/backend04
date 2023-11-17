b90e885810abf9a7249e26d75cf362dea70749de
Nicoly <nicolyvalferreira.564@gmail.com>
2023-11-17 11:13:09 -0300

    feat: arrumando as rotas

--- src/server.js
+++ src/server.js
@@ -1,17 +1,16 @@
 import express from "express";
 import { config } from "dotenv";
+import rotas from "./routes/index.routes.js";
 
 config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 2000;
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.status(200).send({ message: "Hello World!" });
-});
+app.use(rotas);
 
 app.listen(port, () =>
-  console.log(`⚡ Server started on http://localhost:${port}`)
+  console.log(`💋 Server started on http://localhost:${port}`)
 );
